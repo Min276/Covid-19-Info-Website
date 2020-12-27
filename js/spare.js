b3a310c6b405b8da98f42f293788439738f6a88d
Min Khant Kyaw <69049766+Min276@users.noreply.github.com>
2020-12-27 20:48:28 +0630

    COVID-19 Data Update

---
+++ js/spare.js
@@ -0,0 +1,14 @@
+if (self === top) {
+        var antiClickjack = document.getElementById("antiClickjack");
+        antiClickjack.parentNode.removeChild(antiClickjack);
+    } else {
+        top.location = self.location;
+    };
+
+ $(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+    $('nav').addClass('shrink');
+  } else {
+    $('nav').removeClass('shrink');
+  }
+});
