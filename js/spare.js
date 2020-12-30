9456dbcf8c4f3e894a33db45f6b92d188f816678
Min Khant Kyaw <69049766+Min276@users.noreply.github.com>
2020-12-30 20:57:01 +0630

    Data Update

--- js/spare.js
+++ js/spare.js
@@ -1,14 +1,20 @@
 if (self === top) {
-        var antiClickjack = document.getElementById("antiClickjack");
-        antiClickjack.parentNode.removeChild(antiClickjack);
-    } else {
-        top.location = self.location;
-    };
+  var antiClickjack = document.getElementById("antiClickjack");
+  antiClickjack.parentNode.removeChild(antiClickjack);
+} else {
+  top.location = self.location;
+};
 
- $(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('nav').addClass('shrink');
-  } else {
-    $('nav').removeClass('shrink');
-  }
+var myVar = setInterval (myTimer, 5000);
+  function myTimer(){
+  document.getElementById('banner').innerHTML = 'COVID-19 Situation in Myanmar';
+};
+
+$(window).scroll(function() {
+if ($(document).scrollTop() > 50) {
+$('nav').addClass('shrink');
+} else {
+$('nav').removeClass('shrink');
+}
 });
+
